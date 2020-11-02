09ddaba881839228ca864e8f6e7ee5a28756a9fc
Eric D. Torres <etorresnotary@gmail.com>
2020-11-01 23:28:23 -0600

    updated division and type for non-funcitoning PUT calls

--- public/assets/js/put/updateDivision.js
+++ public/assets/js/put/updateDivision.js
@@ -1,25 +1,28 @@
 $(document).ready(function () {
-  $("#create-division").on("submit", (e) => {
+  $("#update-division").on("click", (e) => {
     e.preventDefault();
 
+    const id = $("#update-division").data("id");
     const division = $("#division").val().trim();
     const judgeFName = $("#judgeFName").val().trim();
     const judgeLName = $("#judgeLName").val().trim();
     const roomNumber = $("#roomNumber").val().trim();
 
-    newDivision = {
+    updateDivision = {
       division,
       judgeFName,
       judgeLName,
       roomNumber,
     };
 
-    $.ajax("/api/divisions", {
-      type: "POST",
-      data: newDivision,
+    $.ajax("/api/divisions" + id, {
+      type: "PUT",
+      data: updateDivision,
     }).then(() => {
       location.reload();
-      alert("You've added a new Judge and Division to the database.");
+      alert(
+        "You've updated the information for this Judge and Division in the database."
+      );
     });
   });
 });
---
+++ public/assets/js/put/updateType.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#update-type").on("click", (e) => {
+    e.preventDefault();
+
+    const id = $("#update-type").data("id");
+    const type = $("#type").val().trim();
+
+    updatedType = {
+      Field: type,
+    };
+
+    $.ajax("/api/types"+id, {
+      type: "PUT",
+      data: updatedType,
+    }).then(() => {
+      location.reload();
+      alert("You've updated classification of case in the database.");
+    });
+  });
+});
